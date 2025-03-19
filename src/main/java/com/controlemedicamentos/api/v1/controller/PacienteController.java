6da9bd7783f3e437ca369e8c172e39b1e76c1da5
Alexandre <alevaristofig@gmail.com>
2025-03-18 23:22:16 -0300

    Criação do controller e do metodo listar no service paciente

---
+++ src/main/java/com/controlemedicamentos/api/v1/controller/PacienteController.java
@@ -0,0 +1,32 @@
+package com.controlemedicamentos.api.v1.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.controlemedicamentos.api.v1.assembler.PacienteModelAssembler;
+import com.controlemedicamentos.api.v1.dto.PacienteDTO;
+import com.controlemedicamentos.domain.model.Paciente;
+import com.controlemedicamentos.domain.service.PacienteService;
+
+@RestController
+@RequestMapping(path = "/v1/pacientes", produces = MediaType.APPLICATION_JSON_VALUE)
+public class PacienteController {
+	
+	@Autowired
+	private PacienteService service;
+	
+	@Autowired
+	private PacienteModelAssembler pacienteModelAssembler;
+
+	@GetMapping
+	public List<PacienteDTO> listar(){
+		List<Paciente> pacientes = service.listar();
+		
+		return pacienteModelAssembler.toCollectionModel(pacientes);
+	}
+}
--- src/main/java/com/controlemedicamentos/domain/service/PacienteService.java
+++ src/main/java/com/controlemedicamentos/domain/service/PacienteService.java
@@ -1,5 +1,7 @@
 package com.controlemedicamentos.domain.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,10 @@ public class PacienteService {
 	
 	@Autowired
 	private PacienteInputDisassembler pacienteInputDisassembler;
+	
+	public List<Paciente> listar() {
+		return repository.findAll();
+	}
 	
 	public void salvarPacienteMensagem(PacienteDTO pacienteDTO) {
 		Paciente paciente = pacienteInputDisassembler.toDomainObject(pacienteDTO);
