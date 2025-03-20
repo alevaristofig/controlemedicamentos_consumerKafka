bdbc87371477030eeb452d6f36aa53729333d784
Alexandre <alevaristofig@gmail.com>
2025-03-19 23:11:56 -0300

    Refatoração da classe KafkaConsumerConfig, acrescentou as configurações para o topico medicamentos

--- src/main/java/com/controlemedicamentos/config/KafkaConsumerConfig.java
+++ src/main/java/com/controlemedicamentos/config/KafkaConsumerConfig.java
@@ -16,6 +16,7 @@ import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
+import com.controlemedicamentos.api.v1.dto.MedicamentoDTO;
 import com.controlemedicamentos.api.v1.dto.PacienteDTO;
 import com.controlemedicamentos.api.v1.dto.UsuarioDTO;
 
@@ -63,6 +64,20 @@ public class KafkaConsumerConfig {
 	}
 	
 	@Bean
+	public ConsumerFactory<String, MedicamentoDTO> medicamentoConsumerFactory() {
+		Map<String, Object> props = new HashMap<>();
+		
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, topicMedicamentos);
+		props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+		
+		return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(),
+				new JsonDeserializer<>(MedicamentoDTO.class,false));
+	}
+	
+	@Bean
 	public ConcurrentKafkaListenerContainerFactory<String, UsuarioDTO> kafkaListenerContainerFactoryUsuario() {
 		ConcurrentKafkaListenerContainerFactory<String, UsuarioDTO> factory = new 
 				ConcurrentKafkaListenerContainerFactory<>();
@@ -78,6 +93,16 @@ public class KafkaConsumerConfig {
 				ConcurrentKafkaListenerContainerFactory<>();
 		
 		factory.setConsumerFactory(pacienteConsumerFactory());
+		
+		return factory;
+	}
+	
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, MedicamentoDTO> kafkaListenerContainerFactoryMedicamento() {
+		ConcurrentKafkaListenerContainerFactory<String, MedicamentoDTO> factory = new 
+				ConcurrentKafkaListenerContainerFactory<>();
+		
+		factory.setConsumerFactory(medicamentoConsumerFactory());
 		
 		return factory;
 	}
