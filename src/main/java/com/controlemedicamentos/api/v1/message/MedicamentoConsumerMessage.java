9b17b70aa204f40425d33ee8a068226f22f2f1b1
Alexandre <alevaristofig@gmail.com>
2025-03-19 23:37:53 -0300

    Criação do consumer de medicamento

---
+++ src/main/java/com/controlemedicamentos/api/v1/message/MedicamentoConsumerMessage.java
@@ -0,0 +1,28 @@
+package com.controlemedicamentos.api.v1.message;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.stereotype.Component;
+
+import com.controlemedicamentos.api.v1.dto.MedicamentoDTO;
+import com.controlemedicamentos.domain.service.MedicamentoService;
+
+@EnableKafka
+@Component
+public class MedicamentoConsumerMessage {
+
+	private final Logger logger = LoggerFactory.getLogger(MedicamentoConsumerMessage.class);
+	
+	@Autowired
+	private MedicamentoService service;
+	
+	private final String topic = "medicamentos";
+	
+	public void listening(MedicamentoDTO medicamentoDTO) {
+		logger.info("Mensagem Medicamento recebida "+ medicamentoDTO);
+		
+		service.salvarMedicamentoMensagem(medicamentoDTO);
+	}
+}
--- src/main/java/com/controlemedicamentos/domain/service/MedicamentoService.java
+++ src/main/java/com/controlemedicamentos/domain/service/MedicamentoService.java
@@ -1,5 +1,25 @@
 package com.controlemedicamentos.domain.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.controlemedicamentos.api.v1.assembler.MedicamentoInputDisassembler;
+import com.controlemedicamentos.api.v1.dto.MedicamentoDTO;
+import com.controlemedicamentos.domain.model.Medicamento;
+import com.controlemedicamentos.domain.repository.MedicamentoRepository;
+
+@Service
 public class MedicamentoService {
 
+	@Autowired
+	private MedicamentoRepository repository;
+	
+	@Autowired
+	private MedicamentoInputDisassembler medicamentoInputDisassembler;
+	
+	public void salvarMedicamentoMensagem(MedicamentoDTO medicamentoDTO) {
+		Medicamento medicamento = medicamentoInputDisassembler.toDomainObject(medicamentoDTO);
+		
+		repository.save(medicamento);
+	}
 }
