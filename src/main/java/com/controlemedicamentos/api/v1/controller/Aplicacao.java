d52a356e6629eaa08b2689e3cf302b07134ad1ea
Alexandre <alevaristofig@gmail.com>
2025-03-24 23:08:05 -0300

    Criação do controller de aplicacao

--- src/main/java/com/controlemedicamentos/api/v1/controller/Aplicacao.java
+++ src/main/java/com/controlemedicamentos/api/v1/controller/Aplicacao.java
@@ -1,5 +1,16 @@
 package com.controlemedicamentos.api.v1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.controlemedicamentos.api.v1.assembler.AplicacaoModelAssembler;
+
+@RestController
+@RequestMapping(path = "/v1/aplicacao", produces = MediaType.APPLICATION_JSON_VALUE)
 public class Aplicacao {
 
+	@Autowired
+	private AplicacaoModelAssembler aplicacaoModelAssembler;
 }
