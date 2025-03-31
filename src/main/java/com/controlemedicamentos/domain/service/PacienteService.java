1fc37f8389e2e0cb064e637ecc370c6d02036136
Alexandre <alevaristofig@gmail.com>
2025-03-30 22:33:43 -0300

    Criação do metódo buscarOuFalhar no service Paciente

--- src/main/java/com/controlemedicamentos/domain/service/PacienteService.java
+++ src/main/java/com/controlemedicamentos/domain/service/PacienteService.java
@@ -7,8 +7,11 @@ import org.springframework.stereotype.Service;
 
 import com.controlemedicamentos.api.v1.assembler.PacienteInputDisassembler;
 import com.controlemedicamentos.api.v1.dto.PacienteDTO;
+import com.controlemedicamentos.domain.exception.PacienteNaoEncontradoException;
 import com.controlemedicamentos.domain.model.Paciente;
 import com.controlemedicamentos.domain.repository.PacienteRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class PacienteService {
@@ -23,9 +26,15 @@ public class PacienteService {
 		return repository.findAll();
 	}
 	
+	@Transactional
 	public void salvarPacienteMensagem(PacienteDTO pacienteDTO) {
 		Paciente paciente = pacienteInputDisassembler.toDomainObject(pacienteDTO);
 		
 		repository.save(paciente);
+	}
+	
+	public Paciente buscarOuFalhar(Long id) {
+		return repository.findById(id)
+				.orElseThrow(() -> new PacienteNaoEncontradoException(id));
 	}
 }
